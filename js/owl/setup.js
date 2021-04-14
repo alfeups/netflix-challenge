a3cb6be5dd3d63c1fb2b027140ac0eb617b2ac9e
alfeupereira <alfeups@hotmail.com>
2021-04-14 08:48:38 -0300

    second commit

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,3 +1,4 @@
+/*
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:30,
@@ -13,4 +14,26 @@ $('.owl-carousel').owlCarousel({
             items:5
         }
     }
-})
+}) */
+
+
+$('.owl-carousel').owlCarousel({
+loop:true,
+margin:30,
+nav:true,
+autoplay: true,
+autoPlaySpeed: 900,
+autoPlayTimeout: 900,
+autoplayHoverPause: true,
+responsive:{
+0:{
+    items:1
+},
+600:{
+    items:3
+},
+1000:{
+    items:5
+}
+}
+})      
